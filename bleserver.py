c393d35ea3bc47d6d886fb418ab2ac392673e263
Dan Evans <photonthunder@gmail.com>
2024-10-04 22:15:59 +0100

    class example

--- bleserver.py
+++ bleserver.py
@@ -3,84 +3,113 @@
 import btfpy
 import random
 
-count = 0
-def le_callback(clientnode,operation,cticn):
-  global count
-  if(operation == btfpy.LE_CONNECT):
-    count = 0
-    # clientnode has just connected
-    # pass
-  elif(operation == btfpy.LE_READ):
-    # clientnode has just read local characteristic cticn
-    pass
-  elif(operation == btfpy.LE_WRITE):
-    # clientnode has just written local characteristic cticn
-    data = btfpy.Read_ctic(btfpy.Localnode(),cticn)  # read characteristic to data
-    btfpy.Write_ctic(btfpy.Localnode(),7,[data],0)
-  elif(operation == btfpy.LE_DISCONNECT):
-    # clientnode has just disconnected
-    # uncomment next line to stop LE server when client disconnects
-    # return(btfpy.SERVER_EXIT)
-    # otherwise LE server will continue and wait for another connection
-    # or operation from other clients that are still connected
-    pass
-  elif(operation == btfpy.LE_TIMER):
-    # The server timer calls here every timerds deci-seconds  
-    # clientnode and cticn are invalid
-    # This is called by the server, not a client   
-    # Data (index 6) is notify capable
-    # so if the client has enabled notifications for this characteristic
-    # the following write will send the data as a notification to the client
-    # btfpy.Write_ctic(btfpy.Localnode(),6,[0x67],0)
-    count = count + 5
-    btfpy.Write_ctic(btfpy.Localnode(),5,[count],0)
-    btfpy.Write_ctic(btfpy.Localnode(),6,[random.randint(0,255)],0)
-  elif(operation == btfpy.LE_KEYPRESS):
-    # Only active if btfpy.Keys_to_callback(btfpy.KEY_ON,0) has been called before le_server()
-    # clientnode is invalid
-    # cticn = key code
-    #       = ASCII code of key (e.g. a=97)  OR
-    #         btferret custom code for other keys such as Enter, Home,
-    #         PgUp. Full list in keys_to_callback() section             
-    pass  
-  return(btfpy.SERVER_CONTINUE)
- 
-##### START #####
-  
-if(btfpy.Init_blue("bleserver_devices.txt") == 0):
-  exit(0)
-btfpy.Output_file("bleserver_bug.txt")
-                 # write 0x56 to Info (index 5 in devices.txt)
-                 # find index from UUID = CDEF
-index = btfpy.Find_ctic_index(btfpy.Localnode(),btfpy.UUID_2,[0xCD,0xEF])  # should be 5
-# count = count + 5
-# btfpy.Write_ctic(btfpy.Localnode(),5,count,0)
-                           # local device is allowed to write to its own
-                           # characteristic Info
-                           # Size is known from devices.txt, so last
-                           # parameter (count) can be 0           
-                   # write 0x12 0x34 to Control (index 4)
-btfpy.Write_ctic(btfpy.Localnode(),4,[0x45,0x4E,0x54,0x45, 0x52],0)  
+class BLEServer:
+  def __init__(self):
+    self.count = 0
+    self.local_node = None
+    self.abcd_index = None
+    self.cdef_index = None
+    self.dcba_index = None
+    self.deaf_index = None
 
-btfpy.Keys_to_callback(btfpy.KEY_ON,0)
-                              # OPTIONAL - key presses are sent to le_callback
-                              # with operation=LE_KEYPRESS and cticn=key code 
-                              # The key that stops the server changes from x to ESC
-                              
-# section 3-7-1 Random address alternative setup
-btfpy.Set_le_random_address([0xD3,0x56,0xDB,0x04,0x32,0xA7])
+  def string_to_hex(self, input_string):
+    return [ord(char) for char in input_string]
 
-btfpy.Set_le_wait(5000)   # 5 second wait for connect/pair to complete
+  def initialize(self, device_name="bleserver_devices.txt", bug_name="bleserver_bug.txt"):
+    if(btfpy.Init_blue(device_name) == 0):
+      exit(0)
+    btfpy.Output_file(bug_name)
+    self.local_node = btfpy.Localnode()
+    self.abcd_index = btfpy.Find_ctic_index(self.local_node, btfpy.UUID_2, [0xAB,0xCD])
+    if self.abcd_index != 4:
+      print("Not 4")
+      exit(0)
+    self.cdef_index = btfpy.Find_ctic_index(self.local_node, btfpy.UUID_2, [0xCD,0xEF])
+    if self.cdef_index != 5:
+      print("Not 5")
+      exit(0)
+    self.deaf_index = btfpy.Find_ctic_index(self.local_node, btfpy.UUID_2, [0xDE,0xAF])
+    if self.deaf_index != 6:
+      print("Not 6")
+      exit(0)
+    self.dcba_index = btfpy.Find_ctic_index(self.local_node, btfpy.UUID_2, [0xDC,0xBA])
+    if self.dcba_index != 7:
+      print("Not 7")
+      exit(0)
 
-   # Ask for Just Works security which avoids passkey entry
+    # Have self.abcd show "ENTER" at startup
+    btfpy.Write_ctic(self.local_node, self.abcd_index, self.string_to_hex("ENTER"), 0)  
+
+    btfpy.Keys_to_callback(btfpy.KEY_ON,0)
+                                  # OPTIONAL - key presses are sent to le_callback
+                                  # with operation=LE_KEYPRESS and cticn=key code 
+                                  # The key that stops the server changes from x to ESC
                                   
-btfpy.Le_pair(btfpy.Localnode(),btfpy.JUST_WORKS,0)
+    # section 3-7-1 Random address alternative setup
+    btfpy.Set_le_random_address([0xD3,0x56,0xDB,0x04,0x32,0xA7])
 
-btfpy.Le_server(le_callback,50)
-                              
-# btfpy.Le_server(le_callback,100)
-                   # Become an LE server and wait for clients to connect.   
-                   # when a client performs an operation such as connect, or
-                   # write a characteristic, call the function le_callback()
-                   # Call LE_TIMER in le_callback every 100 deci-seconds (10 seconds)
-btfpy.Close_all()
+    btfpy.Set_le_wait(5000)   # 5 second wait for connect/pair to complete
+
+      # Ask for Just Works security which avoids passkey entry
+    btfpy.Le_pair(self.local_node, btfpy.JUST_WORKS, 0)
+
+  def start_server(self):
+    btfpy.Le_server(lambda clientnode, operation, cticn: self.callback(clientnode, operation, cticn), 50)
+                                  
+    # btfpy.Le_server(le_callback,100)
+                      # Become an LE server and wait for clients to connect.   
+                      # when a client performs an operation such as connect, or
+                      # write a characteristic, call the function le_callback()
+                      # Call LE_TIMER in le_callback every 100 deci-seconds (10 seconds)
+  def close(self):
+    btfpy.Close_all()
+
+  def callback(self, clientnode, operation, cticn):
+    if(operation == btfpy.LE_CONNECT):
+      self.count = 0
+    elif(operation == btfpy.LE_READ):
+      pass
+    elif(operation == btfpy.LE_WRITE):
+      data = btfpy.Read_ctic(self.local_node,cticn)
+      if cticn == self.abcd_index:  # if abcd is written then update dcba
+        btfpy.Write_ctic(self.local_node, self.dcba_index, [data], 0)
+      elif cticn == self.cdef_index:  # Update cdef and write SET CNT to dcba
+        idata = int.from_bytes(data)
+        if 0 <= idata <= 255:
+          btfpy.Write_ctic(self.local_node, self.cdef_index, [idata], 0)
+          self.count = idata
+          btfpy.Write_ctic(self.local_node, self.dcba_index, self.string_to_hex("SET CNT"), 0)
+      else: # Other index write show error
+        btfpy.Write_ctic(self.local_node, self.dcba_index, self.string_to_hex("ERROR"), 0)
+    elif(operation == btfpy.LE_DISCONNECT):
+      # clientnode has just disconnected
+      # uncomment next line to stop LE server when client disconnects
+      # return(btfpy.SERVER_EXIT)
+      # otherwise LE server will continue and wait for another connection
+      # or operation from other clients that are still connected
+      pass
+    elif(operation == btfpy.LE_TIMER):
+      # The server timer calls here every timerds deci-seconds  
+      # clientnode and cticn are invalid
+      # This is called by the server, not a client   
+      self.count = self.count + 5
+      if self.count > 255:
+        self.count = 0
+      btfpy.Write_ctic(self.local_node, self.cdef_index, [self.count], 0)
+      btfpy.Write_ctic(self.local_node, self.deaf_index, [random.randint(0,255)], 0)
+      pass
+    elif(operation == btfpy.LE_KEYPRESS):
+      # Only active if btfpy.Keys_to_callback(btfpy.KEY_ON,0) has been called before le_server()
+      # clientnode is invalid
+      # cticn = key code
+      #       = ASCII code of key (e.g. a=97)  OR
+      #         btferret custom code for other keys such as Enter, Home,
+      #         PgUp. Full list in keys_to_callback() section             
+      pass  
+    return(btfpy.SERVER_CONTINUE)
+
+if __name__ == "__main__":
+  ble_server = BLEServer()
+  ble_server.initialize()
+  ble_server.start_server()
+  ble_server.close()
