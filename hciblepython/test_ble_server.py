017cb706f90640fa14cba455cc7b258783c0dd26
Dan Evans <photonthunder@gmail.com>
2024-11-26 21:54:04 +0000

    Not needed

--- hciblepython/test_ble_server.py
+++ hciblepython/test_ble_server.py
@@ -104,14 +104,6 @@ class BLE(BluetoothLEConnection):
             self.restore_terminal()
 
         # Closing steps
-        # self.do_set_advertise_enable(Advertising.ENABLED)
-        # self.wait_listen(self.long_wait)
-        # self.do_set_advertising_parameters(
-        #     min_interval=0.320,
-        #     max_interval=0.320,
-        #     own_addr_type=Address.PUBLIC # turn off random address
-        # )
-        # self.wait_listen()
         self.do_set_advertise_enable(Advertising.DISABLED)
         self.wait_listen()
 
